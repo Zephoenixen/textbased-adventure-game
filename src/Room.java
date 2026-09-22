ce5c9735522c5ec05f9b86cb31638eed28ed660f
Zephoenixen <157467949+Zephoenixen@users.noreply.github.com>
2026-09-22 14:39:37 +0200

    Room Constructor

--- src/Room.java
+++ src/Room.java
@@ -1,6 +1,13 @@
 public class Room {
     Room northRoom;
-    Room westRoom;
-    Room southRoom;
     Room eastRoom;
+    Room southRoom;
+    Room westRoom;
+
+    Room(Room n, Room e, Room s, Room w){
+        this.northRoom = n;
+        this.eastRoom = e;
+        this.southRoom = s;
+        this.westRoom = w;
+    }
 }
