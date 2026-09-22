c5d7ad9c81731ab28f6f050a974fbef84121810b
matth <code@mfrasmussen.dk>
2026-09-22 15:26:43 +0200

    User interface

--- src/UserInterface.java
+++ src/UserInterface.java
@@ -1,14 +1,17 @@
 public class UserInterface {
-        String kommando = IO.readln("Hvor vil du gå hen? ");
+    public void run() {
+        String kommando = IO.readln();
         switch (kommando) {
-            case "go north", "north", "n" -> IO.println("Going north");
-            case "go west", "west", "w" -> IO.println("Going west");
-            case "go south", "south", "s" -> IO.println("Going south");
-            case "go east", "east", "e" -> IO.println("Going east");
+
+            case "go north" -> IO.println("Going north");
+            case "go south" -> IO.println("Going south");
+            case "go west" -> IO.println("Going west");
+            case "go east" -> IO.println("Going east");
             case "look" -> IO.println("Looking around");
             default -> IO.println("Unkown command");
         }
     }
+}
 
 
 
