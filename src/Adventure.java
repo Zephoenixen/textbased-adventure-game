96400ca0bcff1a26c105a765366c9747c7e2bf1b
ChewOnDat <coding@mfrasmussen.dk>
2026-09-23 13:46:53 +0200

    Har lavet metoder for at tjekke hvilket rum spilleren står i

--- src/Adventure.java
+++ src/Adventure.java
@@ -11,4 +11,40 @@ public class Adventure {
 
         currentRoom = dungeonBuilder.getRoom1();
     }
+    // Tjekker hvis der er et rum vores spillere kan gå ind i. Hvis ik return false.
+    public boolean goNorth(){
+        Room nextRoom = currentRoom.getNorthRoom();
+        if (nextRoom != null) {
+            currentRoom = nextRoom;
+            return true;
+        }
+        return false;
+    }
+    public boolean goEast(){
+        Room nextRoom = currentRoom.getEastRoom();
+        if (nextRoom != null){
+            currentRoom = nextRoom;
+            return true;
+        }
+        return false;
+    }
+    public boolean goSouth(){
+        Room nextRoom = currentRoom.getSouthRoom();
+        if (nextRoom != null) {
+            currentRoom = nextRoom;
+            return true;
+        }
+        return false;
+    }
+    public boolean goWest(){
+        Room nextRoom = currentRoom.getWestRoom();
+        if (nextRoom != null) {
+            currentRoom = nextRoom;
+            return true;
+        }
+        return false;
+    }
+    public  Room getCurrentRoom(){
+        return currentRoom;
+    }
 }
