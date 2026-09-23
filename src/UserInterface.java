9a7f874ddd956976938e3f724687109e1f34ad3b
ChewOnDat <coding@mfrasmussen.dk>
2026-09-23 15:12:38 +0200

    Updated interface

--- src/UserInterface.java
+++ src/UserInterface.java
@@ -16,6 +16,7 @@ public class UserInterface {
                 case "go north" -> {
                     if (adventure.goNorth()) {
                         IO.println("Going north");
+                        IO.println(adventure.currentRoomInfo());
                     } else {
                         IO.println("You cannot go that way");
                     }
@@ -23,6 +24,7 @@ public class UserInterface {
                 case "go east" -> {
                     if (adventure.goEast()) {
                         IO.println("Going east");
+                        IO.println(adventure.currentRoomInfo());
                     } else {
                         IO.println("You cannot go that way");
                     }
@@ -30,6 +32,7 @@ public class UserInterface {
                     case "go south" -> {
                         if (adventure.goSouth()) {
                             IO.println("Going south");
+                            IO.println(adventure.currentRoomInfo());
                         } else {
                             IO.println("You cannot go that way");
                         }
@@ -37,11 +40,12 @@ public class UserInterface {
                         case "go west" -> {
                             if (adventure.goWest()) {
                                 IO.println("Going west");
+                                IO.println(adventure.currentRoomInfo());
                             } else {
                                 IO.println("You cannot go that way");
                             }
                         }
-                            case "look" -> IO.println("Looking around");
+                            case "look" -> IO.println(adventure.currentRoomInfo());
                             case "help" -> IO.println("Help for commands");
                             case "exit" -> running = false;
                             default -> IO.println("Unkown command");
