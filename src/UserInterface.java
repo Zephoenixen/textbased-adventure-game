64b6a485c9b6b697acb40e9ed125fa6645b4f6b6
Zephoenixen <stormriberengelund@gmail.com>
2026-09-23 14:36:18 +0200

    Update UserInterface.java
    Go South Implementation

--- src/UserInterface.java
+++ src/UserInterface.java
@@ -1,12 +1,16 @@
 public class UserInterface {
-    boolean running = true;
+    boolean running;
+    Adventure adventure;
 
     public void run() {
+        running = true;
+        adventure = new Adventure();
+
         while (running){
             String kommando = IO.readln();
             switch (kommando) {
 
-                case "go north" -> IO.println("Going north");
+                case "go north" -> TryGoingSouth();
                 case "go south" -> IO.println("Going south");
                 case "go west" -> IO.println("Going west");
                 case "go east" -> IO.println("Going east");
@@ -17,6 +21,12 @@ public class UserInterface {
             }
         }
         IO.println("goodbye!");
+    }
+
+    private void TryGoingSouth() {
+        if(!adventure.goSouth()){
+            IO.println("There is a wall this way");
+        }
     }
 }
 
