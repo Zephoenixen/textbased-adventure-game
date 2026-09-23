33d0bd34a2bdca75df7ed894589c1cda6bf9632c
ChewOnDat <coding@mfrasmussen.dk>
2026-09-23 14:51:34 +0200

    Updated interface

--- src/UserInterface.java
+++ src/UserInterface.java
@@ -1,24 +1,54 @@
 public class UserInterface {
-    boolean running = true;
+    private Adventure adventure;
+
+    public UserInterface(Adventure adventure) {
+        this.adventure = adventure;
+    }
 
     public void run() {
-        while (running){
+        boolean running = true;
+
+
+        while (running) {
             String kommando = IO.readln();
             switch (kommando) {
 
-                case "go north" -> IO.println("Going north");
-                case "go south" -> IO.println("Going south");
-                case "go west" -> IO.println("Going west");
-                case "go east" -> IO.println("Going east");
-                case "look" -> IO.println("Looking around");
-                case "help" -> IO.println("Help for commands");
-                case "exit" -> running = false;
-                default -> IO.println("Unkown command");
+                case "go north" -> {
+                    if (adventure.goNorth()) {
+                        IO.println("Going north");
+                    } else {
+                        IO.println("You cannot go that way");
+                    }
+                }
+                case "go east" -> {
+                    if (adventure.goEast()) {
+                        IO.println("Going east");
+                    } else {
+                        IO.println("You cannot go that way");
+                    }
+                }
+                    case "go south" -> {
+                        if (adventure.goSouth()) {
+                            IO.println("Going south");
+                        } else {
+                            IO.println("You cannot go that way");
+                        }
+                    }
+                        case "go west" -> {
+                            if (adventure.goWest()) {
+                                IO.println("Going west");
+                            } else {
+                                IO.println("You cannot go that way");
+                            }
+                        }
+                            case "look" -> IO.println("Looking around");
+                            case "help" -> IO.println("Help for commands");
+                            case "exit" -> running = false;
+                            default -> IO.println("Unkown command");
+                        }
+                    }
+                }
             }
-        }
-        IO.println("goodbye!");
-    }
-}
 
 
 
