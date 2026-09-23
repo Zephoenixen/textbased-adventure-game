40b7f9c1bf007b3271eae7feca73bb54ca184d72
Zephoenixen <stormriberengelund@gmail.com>
2026-09-23 14:26:43 +0200

    Filled out template

--- src/UserInterface.java
+++ src/UserInterface.java
@@ -1,17 +1,22 @@
 public class UserInterface {
-    public void run() {
-        String kommando = IO.readln();
-        switch (kommando) {
+    boolean running = true;
 
-            case "go north" -> IO.println("Going north");
-            case "go south" -> IO.println("Going south");
-            case "go west" -> IO.println("Going west");
-            case "go east" -> IO.println("Going east");
-            case "look" -> IO.println("Looking around");
-            case "help" ->
-            case "exit" ->
-            default -> IO.println("Unkown command");
+    public void run() {
+        while (running){
+            String kommando = IO.readln();
+            switch (kommando) {
+
+                case "go north" -> IO.println("Going north");
+                case "go south" -> IO.println("Going south");
+                case "go west" -> IO.println("Going west");
+                case "go east" -> IO.println("Going east");
+                case "look" -> IO.println("Looking around");
+                case "help" -> IO.println("Help for commands");
+                case "exit" -> running = false;
+                default -> IO.println("Unkown command");
+            }
         }
+        IO.println("goodbye!");
     }
 }
 
