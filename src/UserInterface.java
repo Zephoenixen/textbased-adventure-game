78b0d7e7f43b9c57c0dbeb33f8be552c4568048a
Zephoenixen <stormriberengelund@gmail.com>
2026-09-23 15:03:59 +0200

    Cleanup

--- src/UserInterface.java
+++ src/UserInterface.java
@@ -27,28 +27,28 @@ public class UserInterface {
                         IO.println("You cannot go that way");
                     }
                 }
-                    case "go south" -> {
+                case "go south" -> {
                         if (adventure.goSouth()) {
                             IO.println("Going south");
                         } else {
                             IO.println("You cannot go that way");
                         }
                     }
-                        case "go west" -> {
+                case "go west" -> {
                             if (adventure.goWest()) {
                                 IO.println("Going west");
                             } else {
                                 IO.println("You cannot go that way");
                             }
                         }
-                            case "look" -> IO.println("Looking around");
-                            case "help" -> IO.println("Help for commands");
-                            case "exit" -> running = false;
-                            default -> IO.println("Unkown command");
-                        }
-                    }
-                }
+                case "look" -> IO.println("Looking around");
+                case "help" -> IO.println("Help for commands");
+                case "exit" -> running = false;
+                default -> IO.println("Unknown command");
             }
+        }
+    }
+}
 
 
 
