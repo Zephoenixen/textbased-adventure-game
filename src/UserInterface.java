b9c5787cafe25b65fa7131bffc3fef7b5d6dc5b7
Zephoenixen <stormriberengelund@gmail.com>
2026-09-23 15:19:43 +0200

    Cleanup Once More

--- src/UserInterface.java
+++ src/UserInterface.java
@@ -29,30 +29,30 @@ public class UserInterface {
                         IO.println("You cannot go that way");
                     }
                 }
-                    case "go south" -> {
-                        if (adventure.goSouth()) {
-                            IO.println("Going south");
-                            IO.println(adventure.currentRoomInfo());
-                        } else {
-                            IO.println("You cannot go that way");
-                        }
-                    }
-                        case "go west" -> {
-                            if (adventure.goWest()) {
-                                IO.println("Going west");
-                                IO.println(adventure.currentRoomInfo());
-                            } else {
-                                IO.println("You cannot go that way");
-                            }
-                        }
-                            case "look" -> IO.println(adventure.currentRoomInfo());
-                            case "help" -> IO.println("Help for commands");
-                            case "exit" -> running = false;
-                            default -> IO.println("Unkown command");
-                        }
+                case "go south" -> {
+                    if (adventure.goSouth()) {
+                        IO.println("Going south");
+                        IO.println(adventure.currentRoomInfo());
+                    } else {
+                        IO.println("You cannot go that way");
                     }
                 }
+                case "go west" -> {
+                    if (adventure.goWest()) {
+                        IO.println("Going west");
+                        IO.println(adventure.currentRoomInfo());
+                    } else {
+                        IO.println("You cannot go that way");
+                    }
+                }
+                case "look" -> IO.println(adventure.currentRoomInfo());
+                case "help" -> IO.println("Help for commands");
+                case "exit" -> running = false;
+                default -> IO.println("Unkown command");
             }
+        }
+    }
+}
 
 
 
