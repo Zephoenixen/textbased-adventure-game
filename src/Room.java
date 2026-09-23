1aa8731b79e511707dee0869a2271c9cb9c9748a
ChewOnDat <coding@mfrasmussen.dk>
2026-09-23 13:46:34 +0200

    Har lavet metoder for at tjekke hvilket rum spilleren står i

--- src/Room.java
+++ src/Room.java
@@ -5,21 +5,40 @@ public class Room {
     Room southRoom;
     Room westRoom;
 
-    Room(Vector2 ID){
+    Room(Vector2 ID) {
         this.roomID = ID;
     }
 
-    public void AssignNorth(Room room){
+    public void AssignNorth(Room room) {
         this.northRoom = room;
     }
-    public void AssignEast(Room room){
+
+    public void AssignEast(Room room) {
         this.eastRoom = room;
     }
-    public void AssignSouth(Room room){
+
+    public void AssignSouth(Room room) {
         this.southRoom = room;
     }
-    public void AssignWest(Room room){
+
+    public void AssignWest(Room room) {
         this.westRoom = room;
     }
 
+     //Getters for vores Rooms
+    public Room getNorthRoom() {
+        return northRoom;
+    }
+
+    public Room getEastRoom() {
+        return eastRoom;
+    }
+
+    public Room getSouthRoom() {
+        return southRoom;
+    }
+
+    public Room getWestRoom() {
+        return westRoom;
+    }
 }
