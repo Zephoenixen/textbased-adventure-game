4e1f0eb9c0b150b69decceec2b536c088cc6b3c1
Zephoenixen <157467949+Zephoenixen@users.noreply.github.com>
2026-09-24 14:54:34 +0200

    Player CardinalDirections

--- src/Player.java
+++ src/Player.java
@@ -1,3 +1,27 @@
 public class Player {
+    Adventure adv;
 
+    public void GoNorth(){
+        TryGo(adv.goNorth(), "Going North");
+    }
+
+    public void GoEast(){
+        TryGo(adv.goEast(), "Going East");
+    }
+
+    public void GoSouth(){
+        TryGo(adv.goSouth(), "Going South");
+    }
+
+    public void GoWest(){
+        TryGo(adv.goWest(), "Going West");
+    }
+    private void TryGo(boolean condition, String dir_text){
+        if (condition) {
+            IO.println(dir_text);
+            IO.println(adv.currentRoomInfo());
+        } else {
+            IO.println("You cannot go that way");
+        }
+    }
 }
