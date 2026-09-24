18c57b6ab0acab2fb3d987ccec50b168e47e2407
matth <code@mfrasmussen.dk>
2026-09-24 14:48:39 +0200

    test

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 void main() {
+    IO.println("LOOK around for any available door to go through, and GO through to map out the area." + "\n");
     UserInterface userInterface = new UserInterface(new Adventure());
     userInterface.run();
 }
