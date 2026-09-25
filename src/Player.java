dd1ee00fef70722838a170721060054427e3d752
Zephoenixen <stormriberengelund@gmail.com>
2026-09-25 20:18:13 +0200

    Item Taking and Removing without Safety yet Fix

--- src/Player.java
+++ src/Player.java
@@ -35,7 +35,7 @@ public class Player {
 
     public void Look(){
         IO.println("You are in the " + myRoom.getName() + ", it is " + myRoom.getDescription());
-        if(canLoot()) IO.println(myRoom.);
+        if(canLoot()) IO.println(myRoom.getLoot());
     }
 
     public boolean North(){
